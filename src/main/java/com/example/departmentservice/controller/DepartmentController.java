8e372f0a65ca2830553df4d8e0707305e1763cc7
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-02-08 10:23:35 +0530

    added slf4j log

--- src/main/java/com/example/departmentservice/controller/DepartmentController.java
+++ src/main/java/com/example/departmentservice/controller/DepartmentController.java
@@ -2,6 +2,7 @@ package com.example.departmentservice.controller;
 
 import com.example.departmentservice.entity.Department;
 import com.example.departmentservice.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/departments")
+@Slf4j
 public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
@@ -17,6 +19,7 @@ public class DepartmentController {
     @PostMapping("/")
     public Department saveDepartment(@RequestBody Department department)
     {
+        log.info("Inside saveDepartment method of department Controller");
         return departmentService.saveDepartment(department);
     }
 
--- src/main/java/com/example/departmentservice/service/DepartmentService.java
+++ src/main/java/com/example/departmentservice/service/DepartmentService.java
@@ -2,15 +2,18 @@ package com.example.departmentservice.service;
 
 import com.example.departmentservice.entity.Department;
 import com.example.departmentservice.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class DepartmentService {
     @Autowired
     private DepartmentRepository departmentRepository;
 
     public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment method of department Service");
         return departmentRepository.save(department);
     }
 }
